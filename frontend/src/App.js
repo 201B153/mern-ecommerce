6657066dabd7b08d9bdaffdf8bbdfb9aa92b8148
201b153 <tomarm698@gmail.com>
2022-05-13 22:20:04 +0530

    adding-temp.-data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
+import data from './data';
+
 function App() {
   return (
-    <div >
-      <header >
-        <a href="/"> 201b153  </a>
+    <div>
+      <header>
+        <a href="/"> 201b153 </a>
       </header>
       <main>
-        list products
+        <div className="products">
+          <h1>Featured products</h1>
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}</strong></p>
+                <button>Add To Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
