30d9eb30fcf259d30d50c543e6ee79deb8e2b5af
201b153 <tomarm698@gmail.com>
2022-05-13 23:20:17 +0530

    initializing backend

---
+++ backend/README.md
@@ -0,0 +1,9 @@
+# Getting Started With Backend We have To follow steps:
+
+-> in terminal:
+<br/>
+   88  npm init
+   89  npm i express
+   90  node server.js
+   96  npm i nodemon --save-dev
+   98  npm start
---
+++ backend/data.js
@@ -0,0 +1,39 @@
+const data = {
+  products: [
+    {
+      name: 'flower-1',
+      slug: 'flower-1',
+      image: '/assets/pic1.png', //679x x 829x
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      ratings: '4.5',
+      numReviews: 10,
+      description: 'best quality',
+    },
+    {
+      name: 'flower-2',
+      slug: 'flower-2',
+      image: '/assets/pic2.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'hike',
+      ratings: '4.5',
+      numReviews: 10,
+      description: 'best quality',
+    },
+    {
+      name: 'flower-3',
+      slug: 'flower-3',
+      image: '/assets/pic3.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'like',
+      ratings: '4.5',
+      numReviews: 10,
+      description: 'best quality',
+    },
+  ],
+};
+
+export default data;
