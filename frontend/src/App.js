5c5aaad19a7390708e0eb4182256f888ab6ff21f
201b153 <tomarm698@gmail.com>
2022-05-22 01:30:07 +0530

    adding-user-edit-Screen&-it's-functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,6 +32,7 @@ import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreeen from './screens/UserEditScreeen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -233,6 +234,14 @@ function App() {
                 element={
                   <AdminRoute>
                     <UserListScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/user/:id"
+                element={
+                  <AdminRoute>
+                    <UserEditScreeen />
                   </AdminRoute>
                 }
               ></Route>
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useReducer } from 'react';
+import Button from 'react-bootstrap/Button';
 import { Helmet } from 'react-helmet-async';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { Store } from '../Store';
@@ -9,40 +12,48 @@ import { getError } from '../utils';
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FETCH_REQUEST':
-      return {
-        ...state,
-        loading: true,
-      };
+      return { ...state, loading: true };
     case 'FETCH_SUCCESS':
       return {
         ...state,
+        users: action.payload,
         loading: false,
-        orders: action.payload,
       };
-    case 'FECTH_FAIL':
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    case 'DELETE_REQUEST':
+      return { ...state, loadingDelete: true, successDelete: false };
+    case 'DELETE_SUCCESS':
       return {
         ...state,
-        loading: false,
-        error: action.payload,
+        loadingDelete: false,
+        successDelete: true,
       };
+    case 'DELETE_FAIL':
+      return { ...state, loadingDelete: false };
+    case 'DELETE_RESET':
+      return { ...state, loadingDelete: false, successDelete: false };
     default:
       return state;
   }
 };
 export default function UserListScreen() {
+  const navigate = useNavigate();
+  const [{ loading, error, users, loadingDelete, successDelete }, dispatch] =
+    useReducer(reducer, {
+      loading: true,
+      error: '',
+    });
+
   const { state } = useContext(Store);
   const { userInfo } = state;
-  const [{ loading, error, users }, dispatch] = useReducer(reducer, {
-    laoding: true,
-    error: '',
-  });
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         dispatch({ type: 'FETCH_REQUEST' });
-        const { data } = await axios(`/api/users`, {
-          headers: { authorization: `Bearer ${userInfo.token}` },
+        const { data } = await axios.get(`/api/users`, {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
         });
         dispatch({ type: 'FETCH_SUCCESS', payload: data });
       } catch (err) {
@@ -52,14 +63,38 @@ export default function UserListScreen() {
         });
       }
     };
-    fetchData();
-  }, [userInfo]);
+    if (successDelete) {
+      dispatch({ type: 'DELETE_RESET' });
+    } else {
+      fetchData();
+    }
+  }, [userInfo, successDelete]);
+
+  const deleteHandler = async (user) => {
+    if (window.confirm('Are you sure to delete?')) {
+      try {
+        dispatch({ type: 'DELETE_REQUEST' });
+        await axios.delete(`/api/users/${user._id}`, {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        });
+        toast.success('user deleted successfully');
+        dispatch({ type: 'DELETE_SUCCESS' });
+      } catch (error) {
+        toast.error(getError(error));
+        dispatch({
+          type: 'DELETE_FAIL',
+        });
+      }
+    }
+  };
   return (
     <div>
       <Helmet>
-        <title>User</title>
+        <title>Users</title>
       </Helmet>
       <h1>Users</h1>
+
+      {loadingDelete && <LoadingBox></LoadingBox>}
       {loading ? (
         <LoadingBox></LoadingBox>
       ) : error ? (
@@ -72,16 +107,34 @@ export default function UserListScreen() {
               <th>NAME</th>
               <th>EMAIL</th>
               <th>IS ADMIN</th>
-              <th>ACTION</th>
+              <th>ACTIONS</th>
             </tr>
           </thead>
           <tbody>
             {users.map((user) => (
-                <tr key={user._id}>
-                  <td>{user._id}</td>
-                  <td>{user.name}</td>
-                  <td>{user.email}</td>
-                  <td>{user.isAdmin ? 'YES' : 'NO'}</td>
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.isAdmin ? 'YES' : 'NO'}</td>
+                <td>
+                  <Button
+                    type="button"
+                    variant="light"
+                    onClick={() => navigate(`/admin/user/${user._id}`)}
+
+                  >
+                    Edit
+                  </Button>
+                  &nbsp;
+                  <Button
+                    type="button"
+                    variant="light"
+                    onClick={() => deleteHandler(user)}
+                  >
+                    Delete
+                  </Button>
+                </td>
               </tr>
             ))}
           </tbody>
