1a6bff8aeea5170b9d5f404153dbfddc28b9349a
201b153 <tomarm698@gmail.com>
2022-05-23 00:55:54 +0530

    adding-email-response-on-shopping-functionality

--- backend/utils.js
+++ backend/utils.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import mg from 'mailgun-js';
 
 export const generateToken = (user) => {
   return jwt.sign(
@@ -9,7 +10,7 @@ export const generateToken = (user) => {
       isAdmin: user.isAdmin,
     },
     process.env.JWT_SECRET,
-    { expiresIn: '30d', }
+    { expiresIn: '30d' }
   );
 };
 export const isAuth = (req, res, next) => {
@@ -30,9 +31,77 @@ export const isAuth = (req, res, next) => {
 };
 
 export const isAdmin = (req, res, next) => {
-  if (req.user && req.user.isAdmin){
+  if (req.user && req.user.isAdmin) {
     next();
   } else {
-    res.status(401).send({ message: 'Invalid Admin Token'})
+    res.status(401).send({ message: 'Invalid Admin Token' });
   }
-}
+};
+
+export const mailgun = () =>
+  mg({
+    apikey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  });
+
+export const payOrderEmailTemplate = (order) => {
+  return `<h1>Thanks for shopping with Us </h1>
+    <p>
+    Hi ${order.user.name},</p>
+    <p>we have finished processing you order.</p>
+    <h2>[order ${order._id} (${order.createdAt
+    .toString()
+    .substring(0, 10)})</h2>
+    <table>
+    <thead>
+    <tr>
+    <td><strong>Product</strong></td>
+    <td><strong>Quantity</strong></td>
+    <td><strong align="right">Price</strong></td>
+    </tr></thead>
+    <tbody>
+    ${order.orderItems
+      .map(
+        (item) => `
+    <tr>
+    <td>${item.name}</td>
+    <td align="center">${item.quantity}</td>
+    <td align="right"> $${item.price.toFixed(2)}</td>
+    </tr>
+    `
+      )
+      .join('\n')}
+    </tbody>
+    <tfoot>
+    <tr>
+    <td colspan="2">Items Price:</td>
+    <td align="right"> $${order.itemsPrice.toFixed(2)}</td>
+    </tr>
+    <tr>
+    <td colspan="2">shipping Price:</td>
+    <td align="right"> $${order.shippingPrice.toFixed(2)}</td>
+    </tr>
+    <tr>
+    <td colspan="2">Total Price:</td>
+    <td align="right"><strong>$${order.totalPrice.toFixed(2)}</strong> </td>
+    </tr>
+    <tr>
+    <td colspan="2">Payment method</td>
+    <td align="right"> ${order.paymentMethod}</td>
+    </tr>
+    </tfoot>
+    </table>
+    <h2>Shipping Address</h2>
+    <p>
+    ${order.shippingAddress.fullName},<br/>
+    ${order.shippingAddress.address},<br/>
+    ${order.shippingAddress.city},<br/>
+    ${order.shippingAddress.country},<br/>
+    ${order.shippingAddress.postalCode},<br/>
+    </p>
+    <hr/>
+    <p>
+    Thanks for Shopping with us.
+    </p>
+    `;
+};
